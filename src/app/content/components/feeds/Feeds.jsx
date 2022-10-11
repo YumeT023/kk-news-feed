653668a9fee01eeab5c6dd900e2b755c998417c7
Yume <hei.hajatiana@gmail.com>
2022-10-11 21:21:17 +0300

    feat: add feeds layout

---
+++ src/app/content/components/feeds/Feeds.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './feeds.css';
+
+export const Feeds = () => (
+  <div className='content-feeds'>
+    <div className='content-feeds content-feeds_container'></div>
+  </div>
+);
